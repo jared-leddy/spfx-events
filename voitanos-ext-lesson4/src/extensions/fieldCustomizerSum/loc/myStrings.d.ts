fce93806ff75055b089860ab08c99c31bf42b215
Jared Ledbetter <jared.ledbetter@outlook.com>
2022-03-09 15:21:05 -0500

    complete spfx extension lesson 4

---
+++ voitanos-ext-lesson4/src/extensions/fieldCustomizerSum/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IFieldCustomizerSumFieldCustomizerStrings {
+  Title: string;
+}
+
+declare module 'FieldCustomizerSumFieldCustomizerStrings' {
+  const strings: IFieldCustomizerSumFieldCustomizerStrings;
+  export = strings;
+}
